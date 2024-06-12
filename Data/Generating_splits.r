7714bc2f5c67595fdc7c370000b046bf4f68db31
Dylan Dijk <dylan_2103@hotmail.com>
2024-06-12 12:32:28 +0100

    need to fix factor splits

--- Data/Generating_splits.r
+++ Data/Generating_splits.r
@@ -9,7 +9,7 @@ load(url(url))
 
 
 #########################################################################################################################
-# Will have 4 sets of data: Original, Transformed features, Truncated Original, Truncated Transformed Features #
+# Will have 2 sets of data: Original, Transformed features #
 #########################################################################################################################
 
 # For each set we will have 3 data objects:
@@ -24,7 +24,7 @@ load(url(url))
 
 #### Original ####
 info = freMTPL2freq ; claims = freMTPL2sev
-rm(freMTPL2freq);rm(freMTPL2sev)
+# rm(freMTPL2freq);rm(freMTPL2sev)
 # removing outliers
 # for the info dataset truncated the number of claims to 4
 info$ClaimNb_cap = pmin(info$ClaimNb, 4)
@@ -82,24 +82,22 @@ comb_train = comb[setdiff(traincalind,calind),]
  
 
 
-
-
-
-
-
-##################
+##########################################################################################################################################################################
 
 
 #### Transformed features ####
-info_tr = info; claims_tr = claims
+info_tr = freMTPL2freq; claims_tr = freMTPL2sev
 
 # Feature pre-processing for GLM regression
-info_tr$AreaGLM <- as.integer(info_tr$Area)
-info_tr$VehPowerGLM <- as.factor(pmin(info_tr$VehPower,9))
-VehAgeGLM <- cbind(c(0:110), c(1, rep(2,10), rep (3,100)))
-info_tr$VehAgeGLM <- as.factor(VehAgeGLM[info_tr$VehAge+1,2])
-info_tr[,"VehAgeGLM"] <- relevel(info_tr[,"VehAgeGLM"], ref="2")
-DrivAgeGLM <- cbind(
+info_tr$Area <- as.integer(info_tr$Area)
+# truncating vehicle power to 9
+info_tr$VehPower <- as.factor(pmin(info_tr$VehPower,9))
+# Creating 3 groups for the Vehicl age
+VehAge <- cbind(c(0:110), c(1, rep(2,10), rep (3,100)))
+info_tr$VehAge <- as.factor(VehAge[info_tr$VehAge+1,2])
+info_tr[,"VehAge"] <- relevel(info_tr[,"VehAge"], ref="2")
+# Creating 7 groups for the driver age
+DrivAge <- cbind(
   c(18:100),  # Age vector from 18 to 100
   c(
     rep(1, 4),    # Ages 18-21 (4 values)
@@ -111,12 +109,66 @@ DrivAgeGLM <- cbind(
     rep(7, 29)    # Ages 72-100 (29 values)
   )
 )
-info_tr$DrivAgeGLM <- as.factor(DrivAgeGLM[info$DrivAge-17,2])
-info_tr[,"DrivAgeGLM"] <- relevel(info_tr[,"DrivAgeGLM"], ref = "5")
-info_tr$BonusMalusGLM <- as.integer(pmin(info_tr$BonusMalus, 150))
-info_tr$DensityGLM <- as.numeric(log(info_tr$Density))
+info_tr$DrivAge <- as.factor(DrivAge[info$DrivAge-17,2])
+info_tr[,"DrivAge"] <- relevel(info_tr[,"DrivAge"], ref = "5")
+# trunacating BonusMalus to 150
+info_tr$BonusMalus <- as.integer(pmin(info_tr$BonusMalus, 150))
+# log transform for density
+info_tr$Density <- as.numeric(log(info_tr$Density))
 info_tr[,"Region"] <- relevel(info_tr[,"Region"], ref="Centre")
-##################################
+##################
+
+info_tr$ClaimNb_cap = pmin(info_tr$ClaimNb, 4)
+info_tr = select(info_tr, -"ClaimNb")
+# for the claims dataset have removed the top 5% of values
+claims_tr = claims_tr[claims_tr$ClaimAmount < quantile(claims_tr$ClaimAmount, 0.95),]
+# Need to correct the number of claims for each policy now for the combined and aggragated dataset.
+claims_tr = claims_tr %>%
+  group_by(IDpol) %>%
+  mutate(ClaimNb = n()) %>%
+  ungroup()
+
+comb_tr = merge(x = info_tr, y = claims_tr, by = "IDpol", all.x = TRUE)
+comb_tr$ClaimAmount[is.na(comb_tr$ClaimAmount)] = 0
+comb_tr = select(comb_tr, -"ClaimNb_cap")
+comb_tr$ClaimNb = ifelse(is.na(comb_tr$ClaimNb), 0, comb_tr$ClaimNb)
+comb_tr$ClaimNb_cap = pmin(comb_tr$ClaimNb, 4)
+
+comb_agg_tr = comb_tr %>%
+  group_by(IDpol) %>%
+  summarise(ClaimAmount = sum(ClaimAmount))
+
+comb_tr = comb_tr[!comb_tr$ClaimAmount == 0,]
+
+# stratified sampling
+# info dataset
+testzero = info_tr[-traincalzeroind,] 
+calzero = info_tr[calzeroind,] 
+trainzero = info_tr[setdiff(traincalzeroind,calzeroind),] 
+
+testnz = info[-traincalnzind,] 
+calnz = info[calnzind,] 
+trainnz = info[setdiff(traincalnzind,calnzind),] 
+
+info_tr_train = rbind(trainzero, trainnz)
+info_tr_cal = rbind(calzero, calnz)
+info_tr_test = rbind(testzero, testnz)
+
+
+# comb dataset, not stratified sampling as only using data with non zero claims
+comb_tr_test = comb_tr[-traincalind,] 
+comb_tr_cal = comb_tr[calind,] 
+comb_tr_train = comb_tr[setdiff(traincalind,calind),] 
+
+
+
+
+
+
+
+
+
+
 
 
 
